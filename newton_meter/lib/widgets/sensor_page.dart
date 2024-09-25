5b8eee939e3e77538d3c0bef460da34f12861aa3
LeitMoth <col.phillips2004@gmail.com>
2024-09-25 12:55:04 -0500

    record button that calculates something like position

--- newton_meter/lib/widgets/sensor_page.dart
+++ newton_meter/lib/widgets/sensor_page.dart
@@ -9,16 +9,43 @@ class SensorTestPage extends StatefulWidget {
   State<SensorTestPage> createState() => _SensorTestPageState();
 }
 
-
 class _SensorTestPageState extends State<SensorTestPage> {
   StreamSubscription<UserAccelerometerEvent>? userAccelSubscription;
 
   DateTime? lastTime;
   double x = 0, y = 0, z = 0;
+  List<UserAccelerometerEvent> data = [];
+
+  // bool shouldStart = false;
+  bool running = false;
+  // DateTime? recordStart;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Column(children: [
+        FloatingActionButton(
+            child: const Text("hi"),
+            onPressed: () {
+              setState(() {
+                print("Button ${running}!");
+                if (!running) {
+                  print("Starting!");
+                  running = true;
+                  x = 0;
+                  y = 0;
+                  z = 0;
+                }
+                else {
+                  print("Stopping!");
+                  running = false;
+                  lastTime = null;
+                  print("$x, $y, $z");
+                }
+              });
+            })
+      ]),
+    );
   }
 
   @override
@@ -26,16 +53,24 @@ class _SensorTestPageState extends State<SensorTestPage> {
     super.initState();
     userAccelSubscription =
         userAccelerometerEventStream().listen((UserAccelerometerEvent event) {
+      if (running && lastTime == null) {
+        // recordStart = event.timestamp;
+        lastTime = event.timestamp;
+      }
 
-        // https://dart.dev/language/patterns
-        if(lastTime case DateTime last) {
-          var dt = event.timestamp.difference(last).inMicroseconds * 0.000001;
-          x += 0.5*event.x*event.x*dt;
-          y += 0.5*event.y*event.y*dt;
-          z += 0.5*event.z*event.z*dt;
+      // if (recordStart case DateTime start) {
+      //   data.add(event);
+      // }
 
-          print(x);
-        }
+      // https://dart.dev/language/patterns
+      if (lastTime case DateTime last) {
+        var dt = event.timestamp.difference(last).inMicroseconds * 0.000001;
+        x += 0.5 * event.x * event.x * dt;
+        y += 0.5 * event.y * event.y * dt;
+        z += 0.5 * event.z * event.z * dt;
+
+        // print(x);
+      }
     });
   }
 
