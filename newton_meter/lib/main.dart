b686d8c4e62aaa25bf3168127ae897dc5b2aed6d
hspayde <spaydeht@hendrix.edu>
2024-09-25 12:54:37 -0500

    Set up main.dart for testing Calculator.

--- newton_meter/lib/main.dart
+++ newton_meter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newton_meter/Calculator/calculator_main.dart';
 import 'package:newton_meter/widgets/sensor_page.dart';
 
 void main() {
@@ -11,30 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const SensorTestPage();
-    return MaterialApp(
-      title: 'Newton Meter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Newton Meter Home Page'),
-    );
+    return const MaterialApp(home: CalculatorMain());
+    
   }
 }
 
