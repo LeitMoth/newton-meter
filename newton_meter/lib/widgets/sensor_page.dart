080dc51c75a40ae09fe518239c27b738757bfa2a
LeitMoth <col.phillips2004@gmail.com>
2024-09-26 18:02:48 -0500

    record accel, calculate (poorly) velocity and position

--- newton_meter/lib/widgets/sensor_page.dart
+++ newton_meter/lib/widgets/sensor_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
@@ -9,16 +10,86 @@ class SensorTestPage extends StatefulWidget {
   State<SensorTestPage> createState() => _SensorTestPageState();
 }
 
+class Vec3Dt {
+  Vec3Dt({required this.x, required this.y, required this.z, required this.dt});
+  double x = 0, y = 0, z = 0, dt = 0;
+}
+
+class Vec3Time {
+  Vec3Time(
+      {required this.x,
+      required this.y,
+      required this.z,
+      required this.seconds});
+  double x = 0, y = 0, z = 0, seconds = 0;
+}
+
+List<Vec3Time> integrate(List<Vec3Time> vs) {
+  List<Vec3Time> integrated = List.of(vs);
+
+  for(int i = 1; i < vs.length; ++i) {
+    var dt = vs[i].seconds - vs[i-1].seconds;
+    integrated[i].x = integrated[i-1].x + integrated[i].x * dt;
+    integrated[i].y = integrated[i-1].y + integrated[i].y * dt;
+    integrated[i].z = integrated[i-1].z + integrated[i].z * dt;
+  }
+
+  return integrated;
+}
+
+List<double> scalarize(List<Vec3Time> vs) {
+  return vs.map((e) => sqrt(e.x*e.x + e.y*e.y + e.z*e.z)).toList();
+}
+
 class _SensorTestPageState extends State<SensorTestPage> {
   StreamSubscription<UserAccelerometerEvent>? userAccelSubscription;
 
-  DateTime? lastTime;
-  double x = 0, y = 0, z = 0;
-  List<UserAccelerometerEvent> data = [];
+  bool thing = false;
+  // DateTime? lastTime;
+  List<Vec3Time> acceleration = [];
+
+  double px=0,py=0,pz=0;
 
   // bool shouldStart = false;
   bool running = false;
-  // DateTime? recordStart;
+  DateTime? recordStart;
+
+  void record() {
+    if (!running) {
+      running = true;
+      recordStart = null;
+    } else {
+      running = false;
+      recordStart = null;
+      finishRecording();
+    }
+  }
+
+  void finishRecording() {
+      var velocity = integrate(acceleration);
+      var position = integrate(velocity);
+
+      var s_velocity = scalarize(velocity);
+      var s_position = scalarize(position);
+
+      print("Stopping!");
+      if(s_position.isEmpty) {
+        print("s_position was empty!");
+        print("lens ${position.length}, ${velocity.length}, ${acceleration.length}");
+      } else {
+        print(s_position.last);
+
+        px = position.last.x;
+        py = position.last.y;
+        pz = position.last.z;
+      }
+
+      acceleration.clear();
+      velocity.clear();
+      position.clear();
+      s_velocity.clear();
+      s_position.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,23 +98,11 @@ class _SensorTestPageState extends State<SensorTestPage> {
         FloatingActionButton(
             child: const Text("hi"),
             onPressed: () {
-              setState(() {
-                print("Button ${running}!");
-                if (!running) {
-                  print("Starting!");
-                  running = true;
-                  x = 0;
-                  y = 0;
-                  z = 0;
-                }
-                else {
-                  print("Stopping!");
-                  running = false;
-                  lastTime = null;
-                  print("$x, $y, $z");
-                }
-              });
-            })
+              setState(record);
+            }),
+            Text(px.toStringAsPrecision(5)),
+            Text(py.toStringAsPrecision(5)),
+            Text(pz.toStringAsPrecision(5)),
       ]),
     );
   }
@@ -53,24 +112,30 @@ class _SensorTestPageState extends State<SensorTestPage> {
     super.initState();
     userAccelSubscription =
         userAccelerometerEventStream().listen((UserAccelerometerEvent event) {
-      if (running && lastTime == null) {
-        // recordStart = event.timestamp;
-        lastTime = event.timestamp;
+      if (running && recordStart == null) {
+        recordStart = event.timestamp;
+        print("Resetting start time!");
       }
 
-      // if (recordStart case DateTime start) {
-      //   data.add(event);
-      // }
+      if (recordStart case DateTime start) {
+        double elapsed =
+            event.timestamp.difference(start).inMicroseconds * 0.000001;
+        acceleration.add(
+            Vec3Time(x: event.x, y: event.y, z: event.z, seconds: elapsed));
+        // print("Added $elapsed");
+        // data.add(Vec3Dt(x: event.x, y: event.y, z: event.z, dt: dt));
+        // lastTime = event.timestamp;
+      }
 
       // https://dart.dev/language/patterns
-      if (lastTime case DateTime last) {
-        var dt = event.timestamp.difference(last).inMicroseconds * 0.000001;
-        x += 0.5 * event.x * event.x * dt;
-        y += 0.5 * event.y * event.y * dt;
-        z += 0.5 * event.z * event.z * dt;
+      // if (lastTime case DateTime last) {
+      //   var dt = event.timestamp.difference(last).inMicroseconds * 0.000001;
+      //   x += 0.5 * event.x * event.x * dt;
+      //   y += 0.5 * event.y * event.y * dt;
+      //   z += 0.5 * event.z * event.z * dt;
 
-        // print(x);
-      }
+      // print(x);
+      // }
     });
   }
 
